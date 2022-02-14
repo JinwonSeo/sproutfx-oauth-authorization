6159fc6f20faabff4d656ff280507d284d20bbe0
Jinwon Seo <jwseo@live.co.kr>
2022-02-14 18:17:30 +0900

    Closed #10

---
+++ src/main/java/kr/sproutfx/oauth/authorization/api/authorize/exception/MemberAccessDeniedException.java
@@ -0,0 +1,13 @@
+package kr.sproutfx.oauth.authorization.api.authorize.exception;
+
+import org.springframework.http.HttpStatus;
+
+import kr.sproutfx.oauth.authorization.common.exception.BaseException;
+
+public class MemberAccessDeniedException extends BaseException {
+
+    public MemberAccessDeniedException() {
+        super("access_denied", "Access denied.", HttpStatus.UNAUTHORIZED);
+    }
+
+}
