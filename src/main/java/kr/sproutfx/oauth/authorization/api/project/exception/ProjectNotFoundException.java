184f0044a36dda710a00a79c49b56ece3d7334f4
Jinwon Seo <jwseo@live.co.kr>
2022-05-03 16:14:19 +0900

    Import optimization

--- src/main/java/kr/sproutfx/oauth/authorization/api/project/exception/ProjectNotFoundException.java
+++ src/main/java/kr/sproutfx/oauth/authorization/api/project/exception/ProjectNotFoundException.java
@@ -1,8 +1,7 @@
 package kr.sproutfx.oauth.authorization.api.project.exception;
 
-import org.springframework.http.HttpStatus;
-
 import kr.sproutfx.oauth.authorization.common.base.BaseException;
+import org.springframework.http.HttpStatus;
 
 public class ProjectNotFoundException extends BaseException {
 
