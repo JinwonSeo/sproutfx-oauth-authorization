6529161f7c3736f8e448f4476f5543b204572f7b
Jinwon Seo <jwseo@live.co.kr>
2022-05-03 14:20:39 +0900

    Edit indent

--- src/test/java/kr/sproutfx/oauth/authorization/api/client/controller/ClientControllerTest.java
+++ src/test/java/kr/sproutfx/oauth/authorization/api/client/controller/ClientControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -52,26 +53,26 @@ public class ClientControllerTest {
 
     // Mockup recode
     Client mockupClient1 = Client.builder()
-        .id(UUID.fromString("00174dec-5665-4760-9255-6feb51a2b980"))
-        .code("d36c646af3104e4587e6a1dfd7a398d0")
-        .secret("ejRTUHNKTldTcUJiVWh0c2dXRVNlMGhJMDdxWEJLM0M=")
-        .name("Test client #1")
-        .accessTokenSecret("agvHf3DN8zrjM9eyM0lYiV6o3obyacgJCOSm7x2gCgua90whlpSY2AbfcU1pjNEzDzxWPqnqHZVTJdlw5AFku2T9oWlvJEgI")
-        .accessTokenValidityInSeconds(7200L)
-        .status(ClientStatus.ACTIVE)
-        .description(null)
-        .build();
+            .id(UUID.fromString("00174dec-5665-4760-9255-6feb51a2b980"))
+            .code("d36c646af3104e4587e6a1dfd7a398d0")
+            .secret("ejRTUHNKTldTcUJiVWh0c2dXRVNlMGhJMDdxWEJLM0M=")
+            .name("Test client #1")
+            .accessTokenSecret("agvHf3DN8zrjM9eyM0lYiV6o3obyacgJCOSm7x2gCgua90whlpSY2AbfcU1pjNEzDzxWPqnqHZVTJdlw5AFku2T9oWlvJEgI")
+            .accessTokenValidityInSeconds(7200L)
+            .status(ClientStatus.ACTIVE)
+            .description(null)
+            .build();
 
     Client mockupClient2 = Client.builder()
-        .id(UUID.fromString("001cae5a-fd6b-43a5-ba74-07ce7bba256b"))
-        .code("68c2e7fbf4044c6081ab68b742d65a3d")
-        .secret("WHlzaE9oYWtoOElGR0k2SjE5Wnc2Vm5Ybkd2NVF6UVY=")
-        .name("Test client #2")
-        .accessTokenSecret("5iAJvOfyjrMHkYpnAZRUCnnK4ckKBDirfhKGcZAJOLCOvgLMLXHSBB841Lfot9jOXwl5h291GdBQPrE3cL5uxKQJ5FXCSX4D")
-        .accessTokenValidityInSeconds(7200L)
-        .status(ClientStatus.ACTIVE)
-        .description(null)
-        .build();
+            .id(UUID.fromString("001cae5a-fd6b-43a5-ba74-07ce7bba256b"))
+            .code("68c2e7fbf4044c6081ab68b742d65a3d")
+            .secret("WHlzaE9oYWtoOElGR0k2SjE5Wnc2Vm5Ybkd2NVF6UVY=")
+            .name("Test client #2")
+            .accessTokenSecret("5iAJvOfyjrMHkYpnAZRUCnnK4ckKBDirfhKGcZAJOLCOvgLMLXHSBB841Lfot9jOXwl5h291GdBQPrE3cL5uxKQJ5FXCSX4D")
+            .accessTokenValidityInSeconds(7200L)
+            .status(ClientStatus.ACTIVE)
+            .description(null)
+            .build();
 
     Client[] mockupClients = { mockupClient1, mockupClient2 };
 
@@ -79,19 +80,19 @@ public class ClientControllerTest {
     void testFindAll() throws Exception {
         // given
         given(this.clientService.findAll())
-            .willReturn(Lists.newArrayList(mockupClients));
+                .willReturn(Lists.newArrayList(mockupClients));
 
         // when
         ResultActions resultAction = this.mockMvc.perform(get("/clients")
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultAction.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result", Matchers.hasSize(2)));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result", Matchers.hasSize(2)));
     }
 
     @Test
@@ -101,15 +102,15 @@ public class ClientControllerTest {
 
         // when
         ResultActions resultActions = this.mockMvc.perform(get(String.format("/clients/%s", mockupClient1.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.code").exists());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.code").exists());
     }
 
     @Test
@@ -120,30 +121,30 @@ public class ClientControllerTest {
         clientCreateRequest.setDescription(mockupClient1.getDescription());
 
         given(this.clientService.create(clientCreateRequest.getName(), clientCreateRequest.getDescription()))
-            .willReturn(mockupClient1.getId());
+                .willReturn(mockupClient1.getId());
 
         given(this.clientService.findById(mockupClient1.getId()))
-            .willReturn(mockupClient1);
+                .willReturn(mockupClient1);
 
         // when
         ResultActions resultActions = this.mockMvc.perform(post("/clients")
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .content(objectMapper.writeValueAsString(clientCreateRequest))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(objectMapper.writeValueAsString(clientCreateRequest))
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.code").value(mockupClient1.getCode()))
-            .andExpect(jsonPath("result.name").value(mockupClient1.getName()))
-            .andExpect(jsonPath("result.status").value(mockupClient1.getStatus().toString()))
-            .andExpect(jsonPath("result.description").value(mockupClient1.getDescription()));
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.code").value(mockupClient1.getCode()))
+                .andExpect(jsonPath("result.name").value(mockupClient1.getName()))
+                .andExpect(jsonPath("result.status").value(mockupClient1.getStatus().toString()))
+                .andExpect(jsonPath("result.description").value(mockupClient1.getDescription()));
     }
 
     @Test
-    void testUpdate() throws JsonProcessingException, Exception {
+    void testUpdate() throws Exception {
         // given
         ClientUpdateRequest clientUpdateRequest = new ClientUpdateRequest();
         clientUpdateRequest.setName("new name");
@@ -154,62 +155,64 @@ public class ClientControllerTest {
         mockupClient1.setDescription(clientUpdateRequest.getDescription());
         mockupClient1.setAccessTokenValidityInSeconds(clientUpdateRequest.getAccessTokenValidityInSeconds());
 
-        given(this.clientService.findById(mockupClient1.getId())).willReturn(mockupClient1);
+        given(this.clientService.findById(mockupClient1.getId()))
+                .willReturn(mockupClient1);
 
         // when
         ResultActions resultActions = this.mockMvc.perform(put(String.format("/clients/%s", mockupClient1.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .content(objectMapper.writeValueAsString(clientUpdateRequest))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(objectMapper.writeValueAsString(clientUpdateRequest))
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.code").value(mockupClient1.getCode()))
-            .andExpect(jsonPath("result.name").value(mockupClient1.getName()))
-            .andExpect(jsonPath("result.status").value(mockupClient1.getStatus().toString()))
-            .andExpect(jsonPath("result.description").value(mockupClient1.getDescription()));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.code").value(mockupClient1.getCode()))
+                .andExpect(jsonPath("result.name").value(mockupClient1.getName()))
+                .andExpect(jsonPath("result.status").value(mockupClient1.getStatus().toString()))
+                .andExpect(jsonPath("result.description").value(mockupClient1.getDescription()));
     }
 
     @Test
-    void testUpdateStatus() throws JsonProcessingException, Exception {
+    void testUpdateStatus() throws Exception {
         // given
         ClientStatusUpdateRequest clientStatusUpdateRequest = new ClientStatusUpdateRequest();
         clientStatusUpdateRequest.setClientStatus(ClientStatus.ACTIVE);
 
         mockupClient1.setStatus(clientStatusUpdateRequest.getClientStatus());
 
-        given(this.clientService.findById(mockupClient1.getId())).willReturn(mockupClient1);
+        given(this.clientService.findById(mockupClient1.getId()))
+                .willReturn(mockupClient1);
 
         // when
         ResultActions resultActions = this.mockMvc.perform(patch(String.format("/clients/%s/status", mockupClient1.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .content(objectMapper.writeValueAsString(clientStatusUpdateRequest))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(objectMapper.writeValueAsString(clientStatusUpdateRequest))
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.code").value(mockupClient1.getCode()))
-            .andExpect(jsonPath("result.name").value(mockupClient1.getName()))
-            .andExpect(jsonPath("result.status").value(mockupClient1.getStatus().toString()))
-            .andExpect(jsonPath("result.description").value(mockupClient1.getDescription()));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.code").value(mockupClient1.getCode()))
+                .andExpect(jsonPath("result.name").value(mockupClient1.getName()))
+                .andExpect(jsonPath("result.status").value(mockupClient1.getStatus().toString()))
+                .andExpect(jsonPath("result.description").value(mockupClient1.getDescription()));
     }
 
     @Test
     void testDelete() throws Exception {
         // when
         ResultActions resultActions = this.mockMvc.perform(delete(String.format("/clients/%s", mockupClient1.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isNoContent());
+                .andExpect(status().isNoContent());
     }   
 }
--- src/test/java/kr/sproutfx/oauth/authorization/api/project/controller/ProjectControllerTest.java
+++ src/test/java/kr/sproutfx/oauth/authorization/api/project/controller/ProjectControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -53,115 +54,115 @@ public class ProjectControllerTest {
 
     // Mockup recode
     Client mockupClient1 = Client.builder()
-        .id(UUID.fromString("00174dec-5665-4760-9255-6feb51a2b980"))
-        .code("d36c646af3104e4587e6a1dfd7a398d0")
-        .secret("ejRTUHNKTldTcUJiVWh0c2dXRVNlMGhJMDdxWEJLM0M=")
-        .name("Test client #1")
-        .accessTokenSecret("agvHf3DN8zrjM9eyM0lYiV6o3obyacgJCOSm7x2gCgua90whlpSY2AbfcU1pjNEzDzxWPqnqHZVTJdlw5AFku2T9oWlvJEgI")
-        .accessTokenValidityInSeconds(7200L)
-        .status(ClientStatus.ACTIVE)
-        .description(null)
-        .build();
+            .id(UUID.fromString("00174dec-5665-4760-9255-6feb51a2b980"))
+            .code("d36c646af3104e4587e6a1dfd7a398d0")
+            .secret("ejRTUHNKTldTcUJiVWh0c2dXRVNlMGhJMDdxWEJLM0M=")
+            .name("Test client #1")
+            .accessTokenSecret("agvHf3DN8zrjM9eyM0lYiV6o3obyacgJCOSm7x2gCgua90whlpSY2AbfcU1pjNEzDzxWPqnqHZVTJdlw5AFku2T9oWlvJEgI")
+            .accessTokenValidityInSeconds(7200L)
+            .status(ClientStatus.ACTIVE)
+            .description(null)
+            .build();
 
     Client mockupClient2 = Client.builder()
-        .id(UUID.fromString("001cae5a-fd6b-43a5-ba74-07ce7bba256b"))
-        .code("68c2e7fbf4044c6081ab68b742d65a3d")
-        .secret("WHlzaE9oYWtoOElGR0k2SjE5Wnc2Vm5Ybkd2NVF6UVY=")
-        .name("Test client #2")
-        .accessTokenSecret("5iAJvOfyjrMHkYpnAZRUCnnK4ckKBDirfhKGcZAJOLCOvgLMLXHSBB841Lfot9jOXwl5h291GdBQPrE3cL5uxKQJ5FXCSX4D")
-        .accessTokenValidityInSeconds(7200L)
-        .status(ClientStatus.ACTIVE)
-        .description(null)
-        .build();
+            .id(UUID.fromString("001cae5a-fd6b-43a5-ba74-07ce7bba256b"))
+            .code("68c2e7fbf4044c6081ab68b742d65a3d")
+            .secret("WHlzaE9oYWtoOElGR0k2SjE5Wnc2Vm5Ybkd2NVF6UVY=")
+            .name("Test client #2")
+            .accessTokenSecret("5iAJvOfyjrMHkYpnAZRUCnnK4ckKBDirfhKGcZAJOLCOvgLMLXHSBB841Lfot9jOXwl5h291GdBQPrE3cL5uxKQJ5FXCSX4D")
+            .accessTokenValidityInSeconds(7200L)
+            .status(ClientStatus.ACTIVE)
+            .description(null)
+            .build();
 
     Client mockupClient3 = Client.builder()
-        .id(UUID.fromString("0021cf78-8821-4ee7-898c-5f4e8e32ab37"))
-        .code("7689257a6cce464ebeb580cd3c009298")
-        .secret("cVFqYmxMeW42UDNtV3Q1dXl1WFNDUU53UlM5SHg3SlA=")
-        .name("Test client #3")
-        .accessTokenSecret("lPLimNymzrxTTEGwaKB0mmDIzC2HTuFRPGMr2k4IVHc05AUrxYYRc64KB94xkuqXMHuDkDvN7zDe358uMAWChcOFFL8SQI7u")
-        .accessTokenValidityInSeconds(7200L)
-        .status(ClientStatus.ACTIVE)
-        .description(null)
-        .build();
+            .id(UUID.fromString("0021cf78-8821-4ee7-898c-5f4e8e32ab37"))
+            .code("7689257a6cce464ebeb580cd3c009298")
+            .secret("cVFqYmxMeW42UDNtV3Q1dXl1WFNDUU53UlM5SHg3SlA=")
+            .name("Test client #3")
+            .accessTokenSecret("lPLimNymzrxTTEGwaKB0mmDIzC2HTuFRPGMr2k4IVHc05AUrxYYRc64KB94xkuqXMHuDkDvN7zDe358uMAWChcOFFL8SQI7u")
+            .accessTokenValidityInSeconds(7200L)
+            .status(ClientStatus.ACTIVE)
+            .description(null)
+            .build();
 
     Client mockupClient4 = Client.builder()
-        .id(UUID.fromString("00275660-6545-4d40-9c63-3ace62485496"))
-        .code("62012c9a75344076a04d2575e96c4c00")
-        .secret("a2lBTE5mVHhIeGJkY25CcHNxUjhBV3pwcFVIWGJuNmE=")
-        .name("Test client #4")
-        .accessTokenSecret("oG1n9T9jQ23bcIGeMl6ZP1JXqJCsaXnz7LN0TmtbdLpHDrIWNgDioNwzfz7X6vBdNC9rXeWZ1pSM9VjeEDAFccLY0tW3VB4v")
-        .accessTokenValidityInSeconds(7200L)
-        .status(ClientStatus.ACTIVE)
-        .description(null)
-        .build();
+            .id(UUID.fromString("00275660-6545-4d40-9c63-3ace62485496"))
+            .code("62012c9a75344076a04d2575e96c4c00")
+            .secret("a2lBTE5mVHhIeGJkY25CcHNxUjhBV3pwcFVIWGJuNmE=")
+            .name("Test client #4")
+            .accessTokenSecret("oG1n9T9jQ23bcIGeMl6ZP1JXqJCsaXnz7LN0TmtbdLpHDrIWNgDioNwzfz7X6vBdNC9rXeWZ1pSM9VjeEDAFccLY0tW3VB4v")
+            .accessTokenValidityInSeconds(7200L)
+            .status(ClientStatus.ACTIVE)
+            .description(null)
+            .build();
 
     Client[] mockupClients1 = { mockupClient1, mockupClient2 };
     Client[] mockupClients2 = { mockupClient3, mockupClient4 };
 
     Project mockupProject1 = Project.builder()
-        .id(UUID.fromString("000c8538-d62b-41d4-bd0d-f4d4dfb5ad4b"))
-        .name("Test project #1")
-        .status(ProjectStatus.ACTIVE)
-        .description(null)
-        .clients(Lists.newArrayList(mockupClients1))
-        .build();
+            .id(UUID.fromString("000c8538-d62b-41d4-bd0d-f4d4dfb5ad4b"))
+            .name("Test project #1")
+            .status(ProjectStatus.ACTIVE)
+            .description(null)
+            .clients(Lists.newArrayList(mockupClients1))
+            .build();
 
     Project mockupProject2 = Project.builder()
-        .id(UUID.fromString("0043fee4-8746-4628-8aff-1d6d63903691"))
-        .name("Test project #2")
-        .status(ProjectStatus.ACTIVE)
-        .description(null)
-        .clients(Lists.newArrayList(mockupClients2))
-        .build();
+            .id(UUID.fromString("0043fee4-8746-4628-8aff-1d6d63903691"))
+            .name("Test project #2")
+            .status(ProjectStatus.ACTIVE)
+            .description(null)
+            .clients(Lists.newArrayList(mockupClients2))
+            .build();
 
     Project[] mockupProjects = { mockupProject1, mockupProject2 };
 
     Project mockupProject = Project.builder()
-        .id(UUID.fromString("004544c0-766e-4ef6-a1eb-bd0c37100d0c"))
-        .name("Created project name")
-        .status(ProjectStatus.PENDING_APPROVAL)
-        .description(null)
-        .build();
+            .id(UUID.fromString("004544c0-766e-4ef6-a1eb-bd0c37100d0c"))
+            .name("Created project name")
+            .status(ProjectStatus.PENDING_APPROVAL)
+            .description(null)
+            .build();
 
     @Test
     void testFindAll() throws Exception {
         // given
         given(this.projectService.findAll())
-            .willReturn(Lists.newArrayList(mockupProjects));
+                .willReturn(Lists.newArrayList(mockupProjects));
 
         // when
         ResultActions resultActions = this.mockMvc.perform(get("/projects")
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result", Matchers.hasSize(2)))
-            .andExpect(jsonPath("result.$[0].client.name").doesNotExist());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result", Matchers.hasSize(2)))
+                .andExpect(jsonPath("result.$[0].client.name").doesNotExist());
     }
 
     @Test
     void testFindAllWithClients() throws Exception {
         // given
         given(this.projectService.findAllWithClients())
-            .willReturn(Lists.newArrayList(mockupProjects));
+                .willReturn(Lists.newArrayList(mockupProjects));
 
         // when
         ResultActions resultActions = this.mockMvc.perform(get("/projects/clients")
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result", Matchers.hasSize(2)))
-            .andExpect(jsonPath("result.$[0].client.name").doesNotExist());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result", Matchers.hasSize(2)))
+                .andExpect(jsonPath("result.$[0].client.name").doesNotExist());
     }
 
     @Test
@@ -172,42 +173,42 @@ public class ProjectControllerTest {
 
         // given
         given(this.projectService.create(projectCreateRequest.getName(), projectCreateRequest.getDescription()))
-            .willReturn(mockupProject.getId());
+                .willReturn(mockupProject.getId());
 
         given(this.projectService.findById(mockupProject.getId()))
-            .willReturn(mockupProject);
+                .willReturn(mockupProject);
         // when
 
         ResultActions resultActions = this.mockMvc.perform(post("/projects")
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .content(objectMapper.writeValueAsString(projectCreateRequest))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(objectMapper.writeValueAsString(projectCreateRequest))
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.name").value(mockupProject.getName()));
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.name").value(mockupProject.getName()));
     }
 
     @Test
     void testFindById() throws Exception {
         // given
         given(this.projectService.findById(mockupProject.getId()))
-            .willReturn(mockupProject);
+                .willReturn(mockupProject);
         
         // when
         ResultActions resultActions = this.mockMvc.perform(get(String.format("/projects/%s", mockupProject.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.name").value(mockupProject.getName()));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.name").value(mockupProject.getName()));
     }
 
     @Test
@@ -225,17 +226,17 @@ public class ProjectControllerTest {
 
         // when
         ResultActions resultActions = this.mockMvc.perform(put(String.format("/projects/%s", mockupProject.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .content(objectMapper.writeValueAsString(request))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaTypes.HAL_JSON));
         
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.name").value(request.getName()))
-            .andExpect(jsonPath("result.description").value(request.getDescription()));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.name").value(request.getName()))
+                .andExpect(jsonPath("result.description").value(request.getDescription()));
     }
 
     @Test
@@ -251,28 +252,28 @@ public class ProjectControllerTest {
 
         // when
         ResultActions resultActions = this.mockMvc.perform(patch(String.format("/projects/%s/status", mockupProject.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .content(objectMapper.writeValueAsString(request))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("succeeded").value(true))
-            .andExpect(jsonPath("result.status").value(mockupProject.getStatus().toString()));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("succeeded").value(true))
+                .andExpect(jsonPath("result.status").value(mockupProject.getStatus().toString()));
     }
 
     @Test
     void testDelete() throws Exception {
         // when
         ResultActions resultActions = this.mockMvc.perform(delete(String.format("/projects/%s", mockupProject.getId()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .characterEncoding(Charset.forName("UTF-8"))
-            .accept(MediaTypes.HAL_JSON));
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaTypes.HAL_JSON));
 
         // then
         resultActions.andDo(print())
-            .andExpect(status().isNoContent());
+                .andExpect(status().isNoContent());
     }
 }
