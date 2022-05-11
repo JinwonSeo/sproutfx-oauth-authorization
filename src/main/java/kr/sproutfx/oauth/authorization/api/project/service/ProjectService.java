9f37337ca8d986645b559f7438e7515de23d94a8
Jinwon Seo <jwseo@live.co.kr>
2022-05-11 15:25:58 +0900

    Delete craete/update/delete method

--- src/main/java/kr/sproutfx/oauth/authorization/api/project/service/ProjectService.java
+++ src/main/java/kr/sproutfx/oauth/authorization/api/project/service/ProjectService.java
@@ -1,13 +1,11 @@
 package kr.sproutfx.oauth.authorization.api.project.service;
 
 import kr.sproutfx.oauth.authorization.api.project.entity.Project;
-import kr.sproutfx.oauth.authorization.api.project.enumeration.ProjectStatus;
 import kr.sproutfx.oauth.authorization.api.project.exception.ProjectNotFoundException;
 import kr.sproutfx.oauth.authorization.api.project.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,49 +17,11 @@ public class ProjectService {
         this.projectRepository = projectRepository;
     }
 
-    public List<Project> findAll() {
-        return this.projectRepository.findAll();
-    }
-
-    public List<Project> findAllWithClients() {
-        return this.projectRepository.findAllWithClients();
-    }
-
     public Project findById(UUID id) {
+        return this.projectRepository.findById(id).orElseThrow(ProjectNotFoundException::new);
+    }
+
+    public Project findByIdWithClients(UUID id) {
         return this.projectRepository.findByIdWithClients(id).orElseThrow(ProjectNotFoundException::new);
     }
-
-    @Transactional
-    public UUID create(String name, String description) {
-        Project persistenceProject = this.projectRepository.save(Project.builder()
-            .name(name)
-            .description(description)
-            .status(ProjectStatus.PENDING_APPROVAL)
-            .build());
-
-        return persistenceProject.getId();
-    }
-
-    @Transactional
-    public void update(UUID id, String name, String description) {
-        Project persistenceProject = this.projectRepository.findById(id).orElseThrow(ProjectNotFoundException::new);
-
-        persistenceProject.setName(name);
-        persistenceProject.setDescription(description);
-    }
-
-    @Transactional
-    public void updateStatus(UUID id, ProjectStatus projectStatus) {
-        Project persistenceProject = this.projectRepository.findById(id).orElseThrow(ProjectNotFoundException::new);
-
-        persistenceProject.setStatus(projectStatus);
-    }
-
-    @Transactional
-    public void delete(UUID id) {
-        Project persistenceProject = this.projectRepository.findById(id).orElseThrow(ProjectNotFoundException::new);
-
-        this.projectRepository.delete(persistenceProject);
-    }
-
 }
